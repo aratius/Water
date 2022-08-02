8a58f50bedd5093f6f9949f88d35bdf9232098d5
aratius <arata1129matsu@icloud.com>
2022-08-02 09:30:37 +0900

    sin波を実装

--- debug/debug.ino
+++ debug/debug.ino
@@ -1,28 +1,29 @@
 int pins[] = {22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};
 int lines[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 int beforeLines[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+#define PIN_LEN 15
 
 void setup() {
   Serial.begin(115200);
 
-  for(int i = 0; i < 15; i++) {
+  for(int i = 0; i < PIN_LEN; i++) {
     int line = lines[i];
     // pinMode(line, OUTPUT);
   }
 }
 
 void loop() {
-  printCurrent();
-
   int _delay = 0;
   
   // ここからそれぞれのパターン
   // コメントアウトで切り替え
   // 必要なdelayを返してかつステータスを更新する関数
-  _delay = zigzag();
+  // _delay = zigzag();
+  // _delay = sinWave();
   
   compareLines();
   updateStatus();
+  printCurrent();
   delay(_delay);
 }
 
@@ -30,7 +31,7 @@ void loop() {
  * 現在の状態を表示
  */
 void printCurrent() {
-  for(int i = 0;i < 15; i++) {
+  for(int i = 0;i < PIN_LEN; i++) {
     if(lines[i] == 0) Serial.print("_");
     else Serial.print("0");
     Serial.print(" ");
@@ -42,7 +43,7 @@ void printCurrent() {
  * ステータスを比べて変化があればdigitalzWriteする
  */
 void compareLines() {
-  for(int i = 0; i < 15; i++) {
+  for(int i = 0; i < PIN_LEN; i++) {
     int crr = lines[i];
     int last = beforeLines[i];
     // TODO: 状態が変化したらHIGHとLOWを切り替え
@@ -53,11 +54,15 @@ void compareLines() {
 }
 
 void updateStatus() {
-  for(int i = 0; i < 15; i++) {
+  for(int i = 0; i < PIN_LEN; i++) {
     beforeLines[i] = lines[i];
   }
 }
 
+
+/**
+ * ----- ジグザグ -----
+ */
 int zigzagCrrIndex = 0;
 int zigzagDir = 1;
 int zigzag() {
@@ -67,7 +72,7 @@ int zigzag() {
   if((crr <= 0 && dir == -1) || (crr >= 14 && dir == 1)) dir *= -1;
   crr += dir;
   
-  for(int i = 0; i < 15; i++) {
+  for(int i = 0; i < PIN_LEN; i++) {
     if(crr == i) lines[i] = 1;
     else lines[i] = 0;
   }
@@ -75,4 +80,35 @@ int zigzag() {
   zigzagCrrIndex = crr;
   zigzagDir = dir;
   return 100;
+}
+
+
+/**
+ * ----- sin -----
+ */
+int sinDir = 1;
+float sinLast = 0;
+int sinWave() {
+    int dir = sinDir;
+    unsigned long ms = millis();
+    float t = (float)ms * 0.006;
+    float sinCrr = (sin(t) + 1) * 0.5 * (float)PIN_LEN;
+
+    for(int i = 0; i < PIN_LEN; i++) {
+      float pos = (float)i + 0.5;
+      
+      if(
+        dir == 1 && (pos > sinLast && pos <= sinCrr) ||
+        dir == -1 && (pos < sinLast && pos >= sinCrr)
+      ) {
+        lines[i] = 1;
+        if((i == 0 && dir == -1) || (i == PIN_LEN - 1 && dir == 1)) dir *= -1;
+      } else {
+        lines[i] = 0;
+      }
+    }
+
+    sinDir = dir;
+    sinLast = sinCrr;
+    return 50;
 }
